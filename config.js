1be7f0860c19cd8b719d1011dabcd41489ed3c0f
Ghost-rider-HCMUT <114762097+Ghost-rider-HCMUT@users.noreply.github.com>
2026-04-04 21:01:00 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Telegram Configuration
+// This file will be auto-generated during GitHub Actions deployment
+// DO NOT commit real tokens here!
+window.TELEGRAM_BOT_TOKEN = '';
+window.TELEGRAM_CHAT_ID = '';
